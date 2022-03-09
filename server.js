9af18d5006de9f3a00e878ef44acb6f638bdf4b1
Mitchel-DS <mitchel.staal@outlook.com>
2022-03-09 11:04:42 +0100

    minor changes

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const { engine } = require('express-handlebars');
 const express = require('express');
-
 const connectDB = require('./config/db');
-
-require('dotenv').config();
-
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
 connectDB();
 
 app.engine('hbs', engine({
