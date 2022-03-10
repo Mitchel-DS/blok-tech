e601cc9ef66ea947b2acee9c4aa6a15c37bebf00
Mitchel-DS <mitchel.staal@outlook.com>
2022-03-10 23:50:43 +0100

    post model added

---
+++ models/Post.js
@@ -0,0 +1,25 @@
+const  mongoose  = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+
+    firstname:{
+        type:String,
+        required:true,
+    },
+    lastname:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    game:{
+        type:String,
+        required:false
+    }
+})
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post
