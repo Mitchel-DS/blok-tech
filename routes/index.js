0083be7c88501881c7e7e275a1ac1d4bd2acae66
Mitchel-DS <mitchel.staal@outlook.com>
2022-04-08 03:21:38 +0200

    added routing

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const home = require('./home');
 const userpost  = require('./userpost');
+const profile = require('./profile');
+const error = require('./error');
 
 router.use('/', home);
+
 router.use('/userpost', userpost);
+router.use('/profile', profile);
+router.use('*', error);
 
 module.exports = router;
